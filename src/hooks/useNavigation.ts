31b42e15d31b4be2fd7f1df2d303ca0909b9fcbc
Isaac <isaachkdeveloper@gmail.com>
2022-09-02 20:19:19 +0800

    chores: Update useNavigation hook

--- src/hooks/useNavigation.ts
+++ src/hooks/useNavigation.ts
@@ -2,7 +2,6 @@ import { useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { globalActions } from 'store/global';
 import pages from 'config/pages';
-import { projectActions } from 'store/project';
 import { useNavigate } from 'react-router-dom';
 import routes from 'config/routes';
 import globalSelectors from 'store/global/selectors';
@@ -17,25 +16,15 @@ export default function useNavigation(isResponsive: boolean) {
 
   const goToHomeSection = useCallback(() => {
     dispatch(globalActions.setPage(pages.home));
-    dispatch(projectActions.setVisible(false));
     dispatch(globalActions.setShouldShowMenu(false));
-    if (page === pages.projects) {
-      navigate(routes.home, { replace: true });
-    } else if (page === pages.about && isResponsive) {
-      window.scrollTo({
-        top: 0,
-        behavior: 'smooth',
-      });
-    }
+    navigate(routes.home, { replace: true });
     GaService.pageView(pages.home);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [page, isResponsive]);
 
   const goToProjectSection = useCallback(async () => {
-    navigate(routes.home, { replace: true });
-    if (shouldShowMenu) {
-      dispatch(globalActions.setShouldShowMenu(false));
-    }
+    dispatch(globalActions.setPage(pages.projects));
+    dispatch(globalActions.setShouldShowMenu(false));
     navigate(routes.works, { replace: true });
     GaService.pageView(pages.projects);
     // eslint-disable-next-line react-hooks/exhaustive-deps
